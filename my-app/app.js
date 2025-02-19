373ef2941e690470d52949e5c0aa77855046a3e9
danek52 <guskir3565@gmail.com>
2025-02-19 09:46:04 +0300

    add autoreload

--- my-app/app.js
+++ my-app/app.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var reload = require("express-reload");
 var app = express();
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 })
+var path = __dirname + '/app.js'
+app.use(reload(path))
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
