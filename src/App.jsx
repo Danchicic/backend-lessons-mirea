919f3e112a1b1042f17d8a50ded23d075be85840
danek52 <guskir3565@gmail.com>
2025-03-12 10:41:14 +0300

    make practice 9

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Container, Typography} from '@mui/material';
+import ProductList from './components/ProductList';
+import './App.css'
+import {Provider} from "react-redux";
+
+function App() {
+    return (
+        <Container>
+            <Typography variant="h3" component="h1" align="center" sx={{my: 4}}>
+                Интернет-магазин
+            </Typography>
+            <ProductList/>
+        </Container>
+
+    )
+}
+
+export default App
