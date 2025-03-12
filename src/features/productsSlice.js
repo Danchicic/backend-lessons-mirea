8ba9856e54cc44d0ddd42d8da861c83fa54f8891
danek52 <guskir3565@gmail.com>
2025-03-12 11:03:20 +0300

    make practice 10

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -14,8 +14,21 @@ const productsSlice = createSlice({
         items: [],
         status: 'idle',
         error: null,
+        category: 'Все',
+        sortBy: 'default', // Добавляем состояние для сортировки
+        searchQuery: '',
     },
-    reducers: {},
+    reducers: {
+        setCategory: (state, action) => {
+            state.category = action.payload;
+        },
+        setSortBy: (state, action) => {
+            state.sortBy = action.payload;
+        },
+        setSearchQuery: (state, action) => {
+            state.searchQuery = action.payload;
+        },
+    },
     extraReducers: (builder) => {
         builder
             .addCase(loadProducts.pending, (state) => {
@@ -31,4 +44,5 @@ const productsSlice = createSlice({
             });
     },
 });
+export const {setCategory, setSortBy, setSearchQuery} = productsSlice.actions;
 export default productsSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from '../features/productsSlice';
+import CartSlice from "../features/cartSlice.js";
 export const store = configureStore({
     reducer: {
         products: productsReducer,
+        cart:CartSlice,
     },
 });
